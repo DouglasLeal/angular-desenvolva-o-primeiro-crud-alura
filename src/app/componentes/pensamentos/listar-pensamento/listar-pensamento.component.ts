cd30ef205a32d59e634573680d1af3c3a87365a3
Douglas Leal <douglas.leal@live.com>
2023-03-03 08:33:15 -0300

    Usando ngFor e Input para mostrar pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.scss']
 })
 export class ListarPensamentoComponent {
-
+  public listaPensamentos = [
+    {
+      conteudo: "I love Angular",
+      autoria: "Nay",
+      modelo: "modelo3"
+    },
+    {
+      conteudo: "C# >>>>> Java",
+      autoria: "DL",
+      modelo: "modelo1"
+    },
+  ]
 }
