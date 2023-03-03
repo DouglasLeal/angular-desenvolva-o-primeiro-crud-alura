1b96d540c3ea2c6eaa28180454900c41f7a346ea
Douglas Leal <douglas.leal@live.com>
2023-03-03 09:29:02 -0300

    PensamentoService listar criado

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from 'src/app/models/pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.scss']
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit{
   public listaPensamentos: Pensamento[] = []
+
+  constructor(private service: PensamentoService){
+
+  }
+
+  ngOnInit(): void {
+      this.service.listar().subscribe(result => {
+        this.listaPensamentos = result;
+      });
+  }
 }
